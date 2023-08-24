8c082f3ba2e50b2838d603c404cab46284559d4f
arnonsang <arnon.sangn@mail.kmutt.ac.th>
2023-08-25 00:16:54 +0700

    082523 0016 | Add map pin

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import '../app.postcss';
+
+	/** @type {import('@sveltejs/adapter-vercel').Config} */
+	export const config = {
+		runtime: 'nodejs18.x'
+	};
+</script>
+
+<slot />
+<footer class="footer justify-center items-center p-4 bg-neutral text-neutral-content">
+	<div class="items-center grid-flow-col">
+	  
+	  <p>Made with love by <a class="hover:underline" href="https://www.iamickdev.com">ICKDEV❤️</a></p>
+	</div> 
+
+  </footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,58 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { onMount } from 'svelte';
+	import Map from '../components/map.svelte';
+/** @type {import('@sveltejs/adapter-vercel').Config} */
+	export const config = {
+		runtime: 'edge'
+	};
+
+	let marker = {
+		lat: 51.505,
+		lng: -0.09,
+		text: 'You are here',
+		navigate: '/whereami'
+	};
+	let km = 4;
+	let change = false;
+	onMount(() => {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(
+				(position) => {
+					marker.lat = position.coords.latitude;
+					marker.lng = position.coords.longitude;
+					getLocationName();
+				},
+				(error) => {
+					console.log(error);
+				}
+			);
+		} else {
+			console.log('Geolocation is not supported by this browser.');
+		}
+	});
+
+	const getLocationName = async () => {
+		await fetch(
+			`https://nominatim.openstreetmap.org/search?q=${marker.lat},${marker.lng}&format=json`
+		)
+			.then((res) => res.json())
+			.then((data) => {
+				marker.text = data[0].display_name;
+				return data[0].display_name;
+			});
+	};
+</script>
+
+<main class="bg-neutral">
+	<h1 class="text-center text-2xl font-bold text-white py-5">
+		What is my<input
+			type="text"
+			class=" input input-ghost w-12 text-start text-2xl font-bold text-white"
+			bind:value={km}
+			on:change={() => (change = !change)}
+		/>km area?
+	</h1>
+	{#key change}
+		<Map {marker} area={km} />
+	{/key}
+</main>
